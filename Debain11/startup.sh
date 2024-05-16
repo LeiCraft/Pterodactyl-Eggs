4e802b978d53bba2ab05bbca4482a6304f99b6ae
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2024-05-16 23:45:09 +0200

    update

--- Debain11/startup.sh
+++ Debain11/startup.sh
@@ -367,7 +367,7 @@ fi
 #################################
 
 # Download run.sh
-curl -Ls "https://raw.githubusercontent.com/ysdragon/Pterodactyl-VPS-Egg/main/run.sh" -o "$ROOTFS_DIR/home/container/run.sh"
+curl -Ls "https://raw.githubusercontent.com/LeiCraft/Pterodactyl-Eggs/main/Debain11/run.sh" -o "$ROOTFS_DIR/home/container/run.sh"
 # Make run.sh executable.
 chmod +x "$ROOTFS_DIR/home/container/run.sh"
 
