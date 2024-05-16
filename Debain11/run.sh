c007d2d517b7e05c1c332082b84af93f8dd242d0
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2024-05-16 23:43:09 +0200

    update

--- Debain11/run.sh
+++ Debain11/run.sh
@@ -3,12 +3,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-DIR=$PWD # get current dir
-# if current dir is /root print ~
-if [ "$PWD" = "/root"* ]; then
-    DIR="~${PWD#/root}"
-fi
-
 printf "\033c"
 printf "${GREEN}╭────────────────────────────────────────────────────────────────────────────────╮${NC}\n"
 printf "${GREEN}│                                                                                │${NC}\n"
@@ -20,32 +14,4 @@ printf "${GREEN}╰────────────────────�
 printf "                                                                                               \n"
 printf "root@MyVPS:${DIR}#                                                                             \n"
 
-run_cmd() {
-    read -p "root@MyVPS:$DIR# " CMD
-    eval "$CMD"
-
-    # Update DIR after executing command
-    DIR=$PWD
-    if [ "$PWD" = "/root"* ]; then
-        DIR="~${PWD#/root}"
-    fi
-    
-    printf "root@MyVPS:$DIR# \n"
-    run_user_cmd
-}
-
-run_user_cmd() {
-    read -p "user@MyVPS:$DIR# " CMD2
-    eval "$CMD2"
-
-    # Update DIR after executing command
-    DIR=$PWD
-    if [ "$PWD" = "/root"* ]; then
-        DIR="~${PWD#/root}"
-    fi
-    
-    printf "root@MyVPS:$DIR# \n"
-    run_cmd
-}
-
-run_cmd
+service dropbear restart
