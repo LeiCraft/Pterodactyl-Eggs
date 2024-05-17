70d102d7729548ed53bd4d7f69009d383406973d
LeiCraft <linusfisscher08@gmx.net>
2024-05-17 14:08:11 +0200

    update

--- Debain11/entrypoint.sh
+++ Debain11/entrypoint.sh
@@ -6,7 +6,9 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
-curl -Ls -H 'Pragma: no-cache' https://raw.githubusercontent.com/LeiCraft/Pterodactyl-Eggs/main/Debain11/startup.sh -o startup.sh
+curl -Ls -H 'Pragma: no-cache' \
+         -H 'Cache-Control: no-cache, no-store' \
+        https://raw.githubusercontent.com/LeiCraft/Pterodactyl-Eggs/main/Debain11/startup.sh -o startup.sh
 chmod +x ./startup.sh
 # Run the VPS Installer
 bash ./startup.sh
